4f4e416f51213352b19a890fa19632f5953487ca
L AS <as125230as@gmail.com>
2025-08-10 22:51:05 +0800

    新增按鈕內陰以及按壓效果。新增文字漸入與按鈕浮出效果(測試)

--- Kiwi.js
+++ Kiwi.js
@@ -7,3 +7,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+(function checkNav() {
+    const nav = document.querySelector("nav");
+    const section1Height = document.querySelector("#section1").offsetHeight;
+    if (window.scrollY < section1Height - 100) {
+        nav.style.display = "none";
+    } else {
+        nav.style.display = "block";
+    }
+})();
+
+window.addEventListener("scroll", function() {
+    const nav = document.querySelector("nav");
+    const section1Height = document.querySelector("#section1").offsetHeight;
+    if (window.scrollY < section1Height - 100) {
+        nav.style.display = "none";  // 在首頁區域隱藏
+    } else {
+        nav.style.display = "block"; // 超過首頁後顯示
+    }
+});
+
